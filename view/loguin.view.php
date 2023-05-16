a64baa52ffb04300196f3f53d5e17f523607298a
unknown <qelmergabriel@gmail.com>
2023-05-16 11:38:46 -0500

    MODIFIQUE ALGUAS COSAS 1.cambie todo en loguin.view.php 2.cambie todo en registro de datos y hay que agregar la fecha de nacimiento en la base de datos 3.agregue la carpeta img

--- view/loguin.view.php
+++ view/loguin.view.php
@@ -1,20 +1,48 @@
-<?php require "header.php";?>
-<!--header -->
+<?php
+    require "header.php";
+?>
 
-    <h1>asistencia</h1>
 
-    <form action = "loguin.controller.php" method = "post">
-        <div>
-           <span>usuario<input type="usuario"></span>
+<div class="fondo-login">
+    <div class="inicio">
+        <a href="../index.html">
+            <img src="../img/inicio.png" alt="">VUELVA AL INICIO
+        </a>  
+    </div>
+    <div class="titulo">
+        Iniciar Session en Transpotes Puno
+    </div>
+    <form action=" " method="POST" class="col-3 login" autocomplete="off">
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email </label>
+            <input type="email" name="correo" class="form-control" id="exampleInputEmail1" placeholder="user@senati.pe" aria-describedby="emailHelp">
         </div>
-        <div>
-            <span>contrasena<input type="password"></span>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <div class="box-eye">
+                <button type="button" onclick="mostrarContraseña('password','eyepassword')">
+                </button>
+            </div>
+            <input type="password" name="contraseña" class="form-control" id="password">
         </div>
-        <button type="submit">loguearse</button>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Repeat the Password</label>
+            <div class="box-eye">
+                <button type="button" onclick="mostrarContraseña('password2','eyepassword2')">
+                </button>
+            </div>
+            <input type="password" name="confirmarContraseña" class="form-control" id="password2">
+        </div>
+        <div class="d-grid gap-2">
+            <button type="submit" class="btn btn-primary">Login</button>
+        </div>
     </form>
+    <div class="login col-3 mt-3">
+        Si no tiene cuenta registrese por este medio <a href="signup.php" style="text-decoration: none;">Nueva cuenta</a>
+    </div>
+</div>
 
 
-
-
-<!--footer-->
-<?php require "footer.php";?>
+<?php
+    require "footer.php";
+?>
