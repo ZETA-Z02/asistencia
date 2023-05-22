4ecc404bb6499dda9d852e183ba5ca4aaee975d4
ZETA-Z02 <jersson.z032@gmail.com>
2023-05-22 11:32:03 -0500

    conectando

--- controller/editar.php
+++ controller/editar.php
@@ -3,15 +3,20 @@
 require("../model/consultas.model.php");
 
 $consulta = new Consulta();
-
+//que llegue los datos y se actualice con las funciones
 switch(true){
 
     case (isset($_GET['idpersonal'])):
         $id_personal = $_GET['idpersonal'];
         //echo $id_personal;
+        
+
+
+
+
+
 
         break;
-
 
     case (isset($_GET['idcargo'])):
         $id_cargo = $_GET['idcargo'];
@@ -19,13 +24,11 @@ switch(true){
 
         break;
 
-
     case (isset($_GET['idlogin']));
         $id_login = $_GET['idlogin'];
         //echo $id_login;
-        
-        break;
 
+        break;
 
     default:
 
