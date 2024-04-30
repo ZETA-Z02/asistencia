3c61a06d5932c40efc581ef2d8185cec0266ee22
zeta <jersson.z032@gmail.com>
2024-04-30 18:14:45 -0500

    +++

--- model/conexion.php
+++ model/conexion.php
@@ -8,8 +8,8 @@ class Conexion{
     public function __construct(){
         
         $host = "localhost"; 
-        $user = "root";
-        $password = "zeta"; 
+        $user = "jersson";
+        $password = "jersson"; 
         $db = "transportes";
 
         $this->conn=new mysqli($host, $user,$password ,$db);
